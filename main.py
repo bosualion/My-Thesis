09c028bb891fdf21ba0c4e92d80a1accd49f3786
bosualion <hoaingu02@gmail.com>
2024-09-15 21:21:08 +0700

    Thesis

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import webbrowser as wb
+st.title("My Thesis")
+st.image("C:\\Users\\WINDOWS\\Downloads\\43851700_2274373902782883_7644622284539297792_n.jpg")
+st.write("This website is about my research about my Graduate Thesis. Hope everything will be success!")
+st.title("1. Yolo")
+st.image("C:\\Users\\WINDOWS\\Downloads\\yolo-feature-img-1024x576.png")
+st.write("Yolo is a deep learning model, it's application is support for Object Detection.")
+st.write("Yolo Tutorial")
+if st.button("Yolo"):
+    st.write("https://viblo.asia/p/tim-hieu-ve-yolo-trong-bai-toan-real-time-object-detection-yMnKMdvr57P")
+st.title("2. OpenCV")
+st.image("C:\\Users\\WINDOWS\Downloads\\teamvietdev-thu-vien-opencv.jpg")
+st.write("OpenCV is open source library for Image Processing and Computer Vision.")
+st.write("\nHere is it's description. This page has a lots of necessary things about OpenCV functions.")
+st.write("OpenCV Tutorial")
+if st.button("OpenCV"):
+    st.write("https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html")
+st.title("3. Streamlit")
+st.image("C:\\Users\\WINDOWS\\Downloads\\images.png")
+st.write("Streamlit is a python library for web development. This is is one of the fastest ways for python developer to create a website by Python Language.")
+st.write("Streamlit Tutorial")
+if st.button("Streamlit"):
+    st.write("https://streamlitpython.com/")
+st.title("4. ESP32 Cam")
+st.image("C:\\Users\\WINDOWS\\Downloads\\image.jpg")
+st.write("ESP32 Cam is a MCU like ESP32 and also is a programmable camera module")
+st.write("\nESP32 Cam Tutorial")
+if st.button("ESP32 Cam"):
+    st.write("https://randomnerdtutorials.com/esp32-cam-video-streaming-face-recognition-arduino-ide/")
+st.write("\nESP32 Tutorial")
+if st.button ("ESP32"):
+    st.write("https://randomnerdtutorials.com/getting-started-with-esp32/")
+st.write("Sign in to Facebook")
+if st.button("Facebook"):
+    st.write("https://www.facebook.com/")
+
